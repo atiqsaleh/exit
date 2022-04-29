ba6aab3c8e1be88c9846302fa3cec550335eedba
atiqs <atiqs@10.0.0.198>
2022-04-29 12:33:02 -0400

    to gitHub

---
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -0,0 +1,13 @@
+package atiq.com.qa;
+
+public class PushAndPull {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		// i just want to share this project to gitHub
+	}
+
+}
