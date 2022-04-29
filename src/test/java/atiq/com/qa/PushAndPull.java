e6df20a13504cc40d9178f41dbd0a7c701dd96a8
atiqs <atiqs@10.0.0.198>
2022-04-29 14:13:40 -0400

    latest code

--- src/test/java/atiq/com/qa/PushAndPull.java
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -1,37 +1,20 @@
-package atiq.com.qa;
-
-public class PushAndPull {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		String name = "Atiq";
-		System.out.println(name);
-		
-		
-	}	
-	}
+/*
+ * package atiq.com.qa;
+ * 
+ * public class PushAndPull {
+ * 
+ * public static void main(String[] args) { // TODO Auto-generated method stub
+ * 
+ * String name = "Atiq"; System.out.println(name); String name = "Atiq";
+ * System.out.println(name); String name = "Atiq"; System.out.println(name);
+ * String name = "Atiq"; System.out.println(name); String name = "Atiq";
+ * System.out.println(name); String name = "Atiq"; System.out.println(name);
+ * String name = "Atiq"; System.out.println(name); String name = "Atiq";
+ * System.out.println(name); String name = "Atiq"; System.out.println(name);
+ * String name = "Atiq"; System.out.println(name); String name = "Atiq";
+ * System.out.println(name); String name = "Atiq"; System.out.println(name);
+ * String name = "Atiq"; System.out.println(name);
+ * 
+ * 
+ * } }
+ */
