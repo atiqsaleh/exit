a7bac1efb087daf68a644d9ead12de2573232402
atiqs <atiqs@10.0.0.198>
2022-05-01 12:31:18 -0400

    cc

--- src/test/java/atiq/com/qa/PushAndPull.java
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -6,8 +6,11 @@ public class PushAndPull {
 	public static void main(String[] args) {
 
 	  
-		String Firstname = "Arsalan";
+		String Firstname = "Samer";
 		System.out.println(Firstname);
+		
+		String lastname = "siliman";
+		System.out.println(lastname);
 		
 		
 		
