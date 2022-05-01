2a5c33fe839daec0c60d3ee471d35ac8367bb228
atiqsaleh <94880694+atiqsaleh@users.noreply.github.com>
2022-05-01 12:05:38 -0400

    Update PushAndPull.java

--- src/test/java/atiq/com/qa/PushAndPull.java
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -6,10 +6,10 @@
  * public static void main(String[] args) {
  * 
  * 
- * String name = "Saleh"; System.out.println(name); String name = "Saleh";
- * System.out.println(name); String name = "Saleh"; System.out.println(name);
- * String name = "Saleh"; System.out.println(name); String name = "Saleh";
- * System.out.println(name);
+ * String name = "Sdsffaleh";fds dsfdsf.out.println(name); String nsdfdsame = "Saleh";
+ * System.out.pdsfdsrintln(name); String namsdffe = "Saleh"; System.out.println(name);
+ * String name = "dsfsfSaleh"; System.out.pfsdridsfdsfntln(name); String name = "Saleh";
+ * System.out.prdsfdsfintln(name);
  * 
  * 
  * } }
