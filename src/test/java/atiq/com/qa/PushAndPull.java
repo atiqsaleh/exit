18b6a8977321fbe05f3be52f7d83143c7f7ae127
atiqsaleh <94880694+atiqsaleh@users.noreply.github.com>
2022-05-01 11:57:46 -0400

    Update PushAndPull.java

--- src/test/java/atiq/com/qa/PushAndPull.java
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -5,13 +5,9 @@ public class PushAndPull {
 
 	public static void main(String[] args) {
 
-		//dsjfkljjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-		
 	  
 		String name = "Saleh";
 		System.out.println(name);
 		
-		//jdslkfjlk
-		//djslkfjlds
 	}
 }
