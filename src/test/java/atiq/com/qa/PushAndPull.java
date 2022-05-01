5bd702068571367afe2d49a429dbe75eb0fd465c
atiqsaleh <94880694+atiqsaleh@users.noreply.github.com>
2022-04-30 21:51:27 -0400

    add a comments
    added a comments to be pull

--- src/test/java/atiq/com/qa/PushAndPull.java
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -5,6 +5,7 @@ public class PushAndPull {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// i have to practice more and more to overCome to this issue
 
 		
 		
