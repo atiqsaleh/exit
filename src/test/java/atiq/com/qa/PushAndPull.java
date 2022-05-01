3c46d275aa55f0976ea10ac7723dcea97f470e41
atiqsaleh <94880694+atiqsaleh@users.noreply.github.com>
2022-05-01 11:36:21 -0400

    delete 2 line of code

--- src/test/java/atiq/com/qa/PushAndPull.java
+++ src/test/java/atiq/com/qa/PushAndPull.java
@@ -8,12 +8,7 @@ public class PushAndPull {
 		// i want to bring some chages to remote repostorty, go grab the latest code from 
 		// githup
 		
-	     // in the name of god the most mercefull and 
-		// second line to be add to all code
-		// third line should be add too
-	String name = "Atiq";
-		
-		System.out.println(name);
+	  
 		
 		
 	}
